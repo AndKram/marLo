53cab75b7f87484668d70c31a247a962a1d29698
AndKram <v-andkra@microsoft.com>
2018-10-08 17:50:38 +0100

    fix use marlo env

--- test.py
+++ test.py
@@ -13,20 +13,25 @@ def main():
     xml_file = "mission.xml"
     parser = argparse.ArgumentParser(description='Multi-agent test')
     parser.add_argument('--rounds', type=int, default=10, help='number of rollouts - default 10')
-    parser.add_argument('--mission_file', type=str, default=xml_file, help='the mission xml')
+    parser.add_argument('--mission', type=str, default=xml_file, help='the env or mission xml')
+    parser.add_argument('--agent_count', type=int, default=None, help='number of agents')
     parser.add_argument('--server', type=str, default="127.0.0.1", help='the mission server')
     parser.add_argument('--port', type=int, default=10000, help='the mission port')
     args = parser.parse_args()
 
     rounds = args.rounds
-    xml_file = args.mission_file
 
-    xml = etree.parse(xml_file)
-    number_of_agents = len(xml.getroot().findall('{http://ProjectMalmo.microsoft.com}AgentSection'))
+    if args.agent_count is None:
+        xml_file = args.mission
+        xml = etree.parse(xml_file)
+        number_of_agents = len(xml.getroot().findall('{http://ProjectMalmo.microsoft.com}AgentSection'))
+    else:
+        number_of_agents = args.agent_count
 
-    join_tokens = marlo.make(xml_file, params=dict(server=args.server, port=args.port, turn_based=True,
-                                                   comp_all_commands=['move', "turn", "use", "attack"],
-                                                   kill_clients_after_num_rounds=250))
+    join_tokens = marlo.make(args.mission,
+                             params=dict(server=args.server, port=args.port, turn_based=True,
+                                         comp_all_commands=['move', "turn", "use", "attack"]
+                                         ))
 
     def dump():
         while True:
